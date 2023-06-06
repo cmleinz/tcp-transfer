da3c8f124ccd46044ef17742db40e2436dd13bb5
Caleb Leinz <caleb.leinz@atlasground.com>
2023-06-06 09:43:58 -0700

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use anyhow::Result;
 use tokio::{
     fs::File,
-    io::BufReader,
+    io::{BufReader, BufWriter},
     net::{TcpListener, TcpStream},
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = std::env::args().skip(1).collect::<Vec<String>>();
+    println!("Args: {:?}", args);
     match args[0].as_str() {
         "send" => send(&args).await,
         "recv" => recv(&args).await,
@@ -22,16 +23,17 @@ async fn send(args: &[String]) -> Result<()> {
     }
 
     let mut file = BufReader::new(File::open(&args[1]).await?);
-    let mut socket = TcpListener::bind("0.0.0.0:8080").await?.accept().await?;
+    let (socket, _addr) = TcpListener::bind("0.0.0.0:8080").await?.accept().await?;
+    let mut socket = BufWriter::new(socket);
 
-    tokio::io::copy_buf(&mut file, &mut socket.0).await?;
+    tokio::io::copy_buf(&mut file, &mut socket).await?;
     Ok(())
 }
 
 async fn recv(args: &[String]) -> Result<()> {
-    let mut file = BufReader::new(File::create(&args[2]).await?);
-    let mut socket = TcpStream::connect(&args[1]).await?;
+    let mut file = BufWriter::new(File::create(&args[2]).await?);
+    let mut socket = BufReader::new(TcpStream::connect(&args[1]).await?);
 
-    tokio::io::copy_buf(&mut file, &mut socket).await?;
+    tokio::io::copy_buf(&mut socket, &mut file).await?;
     Ok(())
 }
