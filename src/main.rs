feb63d86bd9e2c923982a449c4912358e8ed3da1
Caleb Leinz <caleb.leinz@atlasground.com>
2023-06-08 11:01:27 -0700

    Add some stdout info

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use tokio::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    if std::env::args().len() != 3 {
+        println!("Usage:\ntcp-transfer send FILE_SOURCE LOCAL_SOCKET\ntcp-transfer recv EXTERNAL_SOCKET FILE_DESTINATION");
+        return Ok(());
+    }
     let args = std::env::args().skip(1).collect::<Vec<String>>();
     println!("Args: {:?}", args);
     match args[0].as_str() {
@@ -23,17 +27,30 @@ async fn send(args: &[String]) -> Result<()> {
     }
 
     let mut file = BufReader::new(File::open(&args[1]).await?);
-    let (socket, _addr) = TcpListener::bind("0.0.0.0:8080").await?.accept().await?;
+    let (socket, addr) = TcpListener::bind(&args[2]).await?.accept().await?;
+    println!("Accepted connection from: {addr:?}");
     let mut socket = BufWriter::new(socket);
 
+    println!("Beginning transmit of: {}", args[1]);
     tokio::io::copy_buf(&mut file, &mut socket).await?;
     Ok(())
 }
 
 async fn recv(args: &[String]) -> Result<()> {
-    let mut file = BufWriter::new(File::create(&args[2]).await?);
     let mut socket = BufReader::new(TcpStream::connect(&args[1]).await?);
+    println!("Established connections to: {}", args[1]);
+    let mut file = BufWriter::new(File::create(&args[2]).await?);
 
+    println!("Beginning save of: {file:?}");
+    let recv_time = std::time::Instant::now();
     tokio::io::copy_buf(&mut socket, &mut file).await?;
+    if let Ok(meta) = file.into_inner().metadata().await {
+        println!(
+            "Successfully copied: {}\n\tRun duration: {:.2}(s)\n\tFile size: {} bytes",
+            args[2],
+            recv_time.elapsed().as_secs_f64(),
+            meta.len()
+        );
+    }
     Ok(())
 }
